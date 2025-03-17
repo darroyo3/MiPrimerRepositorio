97853991295b4253608053ddf40ae846a2e1a23f
David Arroyo <darroyo3@educa.madrid.org>
2025-03-17 19:49:12 +0100

    Version inicial

---
+++ MiPrimerRepositorio/src/miprimerrepositorio/MiPrimerRepositorio.java
@@ -0,0 +1,10 @@
+package miprimerrepositorio;
+
+public class MiPrimerRepositorio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Este es el primer repositorio de David");
+	}
+
+}
